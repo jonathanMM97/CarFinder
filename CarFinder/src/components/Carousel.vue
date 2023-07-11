b4bda53ad476d8fae9be5868501efe5ebb438b52
jonathanMM97 <jonathan_tuenty@hotmail.com>
2023-07-11 22:59:06 +0200

    Update the questionnaire to carousel with images(not complete)

---
+++ CarFinder/src/components/Carousel.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="carousel">
+    <slot :currentSlide="currentSlide" :nextSlide="nextSlide" :finished="finished"></slot>
+
+    <!-- Navigation -->
+    <div class="navigate">
+      <div v-show="currentSlide != 1 && currentSlide != 6" class="toggle-page left">
+        <i @click="prevSlide" class="fas fa-chevron-left"></i>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+
+export default {
+  data() {
+    return {
+      currentSlide: 1,
+      getSlideCount: null,
+      finished: false
+    }
+  },
+  mounted() {
+    this.getSlideCount = document.querySelectorAll(".container").length;
+  },
+  methods: {
+    prevSlide() {
+      if (this.currentSlide === 1) {
+        this.currentSlide = 1;
+        return;
+      }
+      this.currentSlide -= 1;
+    },
+    nextSlide() {
+      if (this.currentSlide === 5) {
+        this.finished = true;
+        return;
+      }
+      this.currentSlide += 1;
+    }
+  }
+}
+</script>
+
+
+
+<style lang="scss" scoped>
+
+.navigate {
+  padding: 0 16px;
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .toggle-page {
+    display: flex;
+    flex: 1;
+  }
+
+  i {
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50%;
+    width: 40px;
+    height: 40px;
+    background-color: rgba(0, 0, 0, 0.8);
+    color: #fff;
+  }
+}
+
+</style>
--- CarFinder/src/views/QuestionsView.vue
+++ CarFinder/src/views/QuestionsView.vue
@@ -1,29 +1,44 @@
 <template>
-<h1>Preguntas</h1>
+  <Carousel v-slot="{ currentSlide, nextSlide, finished }">
+    <div v-show="currentSlide  === i + 1" class="container" v-for="(item, i) in list" :key="i" v-bind:style="{'background-image': 'url(' + image[i] + ')'}">
+      <h1>{{ i+1 }}. {{ item.text }}</h1>
 
-<div class="container" v-for="(item, i) in list" :key="i">
-  <section>
-    <h2>{{ i+1 }}. {{ item.text }}</h2>
-
-    <div v-for="(answer, j) in item.answers" :key="j">
-      <label :for="'question_' + i + '_answer_' + j">
-        <input type="radio" :id="'question_' + i + '_answer_' + j" :value="answer.id" :name="'question_' + i" v-model="checked[i]">
-        
-        <span>{{ answer.text }}</span>
-      </label>
+      <div class="q-answer">
+        <div v-for="(answer, j) in item.answers" :key="j">
+          <label :for="'question_' + i + '_answer_' + j">
+            <input type="radio" @click="nextSlide" :id="'question_' + i + '_answer_' + j" :value="answer.id" :name="'question_' + i" v-model="checked[i]">
+              <span>{{ answer.text }}</span>
+          </label>
+        </div>
+      </div>
+      <div class="send"  v-show="finished && currentSlide === 5">
+        <router-link @click="sendAnswer" class="btn btn__link" to="/manage">Terminar</router-link>
+      </div>
     </div>
-  </section>
-</div>
-<button v-on:click="sendAnswer()">Enviar</button>
+  </Carousel>
 </template>
 
 <script>
 import axios from 'axios'
+import Carousel from '../components/Carousel.vue'
+
+import clio from "../assets/clio.png"
+import diesel from "../assets/diesel.png"
+import expensive from "../assets/expensive.png"
+import jeep4x4 from "../assets/jeep4x4.png"
+import kilometres from "../assets/kilometres.png"
+import manual from "../assets/manual.png"
+import monovolumen from "../assets/monovolumen.png"
+import priceCar from "../assets/priceCar.png"
+import security from "../assets/security.png"
+import sport from "../assets/sportCar.png"
+
 
 export default {
-
   data() {
     return {
+      currentSlides: null,
+      image: [clio, diesel, expensive, jeep4x4, kilometres, manual, monovolumen, priceCar, security, sport],
       checked: [],
       list:[]
     }
@@ -40,33 +55,38 @@ export default {
     let result = await axios.get("http://localhost:8080/quiz/round");
     console.warn(result.data);
     this.list = result.data;
-  }
+  },
+  components: {Carousel},
 };
 </script>
 
 <style lang="scss" scoped>
 
-h1 {
-  display: flex;
-  justify-content: center;
+.container {
+  position: absolute;
   margin-top: 10rem;
+  background-size: cover;
+  background-position: center;
+  width: 100%;
+  height: 100vh;
 }
 
-.container {
-  max-width: 700;
-  margin: auto;
-  padding: 20px;
-}
-
-section{
-  border: 2px solid rgba(0, 0, 0, 0.8);
-  border-radius: 7px;
-  padding: 10px;
-  margin: 10px 0;
-}
-
-h2 {
+h1 {
   text-align: center;
+  color: #fff;
+}
+
+.q-answer {
+  position: relative;
+  z-index: 1;
+  background: rgba(0, 0, 0, 0.4);
+  border-radius: 31px;
+  justify-content: center;
+  margin: 10px 30vh;
+  margin-top: 35rem;
+  padding: 8px;
+  display: flex;
+  gap: 10px;
 }
 
 input[type="radio"] {
@@ -81,12 +101,15 @@ label {
 }
 
 label span{
+  text-transform: uppercase;
   display: inline-flex;
   align-items: center;
   padding: 10px 20px 10px 10px;
   border-radius: 31px;
+  color: #fff;
   transition: 0.25s;
 }
+
 
 label span:hover,
 input[type="radio"]:checked + span{
@@ -95,37 +118,33 @@ input[type="radio"]:checked + span{
 }
 
 label span:before {
-  content: "";
-  background: #fff;
-  width: 16px;
-  height: 16px;
-  border-radius: 50%;
-  margin-right: 10px;
-  transition: 0.25s ease all;
-  box-shadow: inset 0 0 0 2px rgba(0, 0, 0, 0.8);
+  visibility: hidden;
 }
 
 input[type="radio"]:checked + span:before {
   box-shadow: inset 0 0 0 10px #00005c;
 }
-
-button {
-  margin-left: 50%;
-  margin-top: 2rem;
-  margin-bottom: 2rem;
-  height: 50%;
-  border: 1px solid rgba(0, 0, 0, 0.8);
-  border-radius: 25px;
-  font-size: 18px;
-  color: rgba(0, 0, 0, 0.8);
-  font-weight: 700;
-  cursor: pointer;
-  outline: none;
-}
-
-button:hover {
-  background: rgba(0, 0, 0, 0.8);
-  color:#fff;
+.send {
+  .btn{
+      background: rgba(0, 0, 0, 0.8);
+      padding: 10px 20px;
+      top: 1rem;
+      left: 50%;
+      font-size: 20px;
+      font-family: 'Montserrat';
+      text-decoration: none;
+      cursor: pointer;
+      border-radius: 10%;
+      transition: 0.8s;
+      position: relative;
+      z-index: 1;
+      overflow: hidden;
+      color: #fff;
+    }
+    .btn__link:hover{
+      background: #00afea;
+      color: rgba(0, 0, 0, 0.8);
+    }
 }
 
 </style>
