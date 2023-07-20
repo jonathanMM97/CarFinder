c539609d725c70957fcbe8d70ebdf94e1ca8edcb
jonathanMM97 <jonathan_tuenty@hotmail.com>
2023-07-20 15:02:18 +0200

    Added functionality of change image for each answer

--- src/views/QuestionsView.vue
+++ src/views/QuestionsView.vue
@@ -1,215 +1,52 @@
 <template>
-  <Carousel v-show="!showProducts" v-slot="{ currentSlide, nextSlide, finished }">
-    <div v-show="currentSlide  === i + 1" class="container" v-for="(item, i) in list" :key="i" v-bind:style="{'background-image': 'url(' + getImage() + ')'}">
-      <h1>{{ i+1 }}. {{ item.text }}</h1>
-
-      <div class="q-answer">
-        <div v-for="(answer, j) in item.answers" :key="j">
-          <label :for="'question_' + i + '_answer_' + j" @mouseover="changeImage(answer.text)">
-            <input type="radio" @click="nextSlide" :id="'question_' + i + '_answer_' + j" :value="answer.id" :name="'question_' + i" v-model="checked[i]">
-              <span>{{ answer.text }}</span>
-          </label>
-        </div>
+  <Carousel :valorProp="1" :QuestionId="0" v-slot="{ vehicules }" v-show="!newCarousel">
+    <div v-show="true" class="show">
+      <div class="nav-search">
+        <button @click="showCarouselAgain" class="more-questions">Búsqueda mas avanzada</button>
       </div>
-
-
-      <div class="send"  v-show="finished && currentSlide === 5">
-        <button @click="sendAnswer" class="btn btn__link" >Terminar</button>
+      <div class="product" v-show="!newCarousel">
+        <a v-for="vehiculo in vehicules" :key="vehiculo.id" :href="vehiculo.link" class="product">
+          <div class="product-container">
+            <img class="item" :src="vehiculo.image">
+            <div class="product__info">
+              <h1>{{ vehiculo.title }}</h1>
+              <h2>{{ vehiculo.price.toLocaleString('en', { useGrouping: true, minimumFractionDigits: 0, maximumFractionDigits: 0, minimumIntegerDigits: 1 }) }} €</h2>
+            </div>
+          </div>
+        </a>
       </div>
     </div>
   </Carousel>
 
-  <div class="show">
-    <div class="nav-search">
-      <button class="more-questions">Continuar respondiendo preguntas</button>
-    </div>
-    <div class="product" v-show="showProducts">
-      <a v-for="vehicules in vehicules" :key="vehicules.id" :href="vehicules.link" class="product">
-        <div class="product-container">
-          <img class="item" :src="vehicules.image">
-          <div class="product__info">
-            <h1>{{ vehicules.title }}</h1>
-            <h2>{{ vehicules.price.toLocaleString('en', { useGrouping: true, minimumFractionDigits: 0, maximumFractionDigits: 0, minimumIntegerDigits: 1 }) }} €</h2>
-          </div>
-        </div>
-      </a>
-    </div>
-  </div>
-
+  <div v-show="newCarousel">
+        <Carousel :valorProp="2" :QuestionId="5" class="advanceProduct"></Carousel>
+      </div>
 </template>
 
 <script>
-import axios from 'axios'
 import Carousel from '../components/Carousel.vue'
 
-import clio from "../assets/clio.png"
-import diesel from "../assets/diesel.png"
-import expensive from "../assets/expensive.png"
-import jeep4x4 from "../assets/jeep4x4.png"
-import kilometres from "../assets/kilometres.png"
-import manual from "../assets/manual.png"
-import monovolumen from "../assets/monovolumen.png"
-import priceCar from "../assets/priceCar.png"
-import security from "../assets/security.png"
-import sport from "../assets/sportCar.png"
+
 
 
 export default {
+  components: {Carousel},
   data() {
     return {
-      index: 9,
-      showProducts: false,
       currentSlides: null,
-      image: clio,
-      logos: ["../assets/autoscout.png", "../assets/coches77.png", "../assets/cochesnet.png", "../assets/keycar.png", "../assets/motores.png", "../assets/motorgiga.png", "../assets/wallapop.png"],
-      checked: [],
-      list:[],
-      vehicules: []
+      newCarousel: false,
     }
   },
-  methods:{
-    async sendAnswer() {
-        await axios.post("http://localhost:8080/quiz/answer/bulk",this.checked)
-        .then(response => this.vehicules = response.data);
-        console.log(this.vehicules.images);
-        this.showProducts = true;
-    },
-    changeImage(answerText){
-      switch(true) {
-        case answerText.includes('Elegante'): this.image = expensive;
-        break;
-        case answerText.includes('funcional'): this.image = clio;
-        break;
-        case answerText.includes('Deportivo'): this.image = sport;
-        break;
-        case answerText.includes('Confort'): this.image = monovolumen;
-        break;
-        case answerText.includes('Velocidad'): this.image = sport;
-        break;
-        case answerText.includes('libertad'): this.image = jeep4x4;
-        break;
-        case answerText.includes('ahorro'): this.image = clio;
-        break;
-        case answerText.includes('Ciudad'): this.image = clio;
-        break;
-        case answerText.includes('Carreteras'): this.image = kilometres;
-        break;
-        case answerText.includes('terreno'): this.image = jeep4x4;
-        break;
-        case answerText.includes('Elegante'): this.image = expensive;
-        break;
-      }
-    },
-    getImage() {
-        return this.image;
-    },
-  }
-  ,
-  async mounted(){
-    let result = await axios.get("http://localhost:8080/quiz/round");
-    this.list = result.data;
-    console.log(result.data);
+  methods: {
+    showCarouselAgain() {
+      this.newCarousel = true;
+    }
   },
-  components: {Carousel},
 };
 </script>
 
 <style lang="scss" scoped>
 
-.container {
-  position: absolute;
-  background-size: cover;
-  background-position: center;
-  top: 6.7rem;
-  left: 0;
-  width: 100%;
-  height: 100vh;
-
-  h1 {
-    font-size: 32px;
-    text-transform: uppercase;
-    text-align: center;
-    color: #fff;
-  }
-
-  .q-answer {
-    position: relative;
-    z-index: 1;
-    background: rgba(0, 0, 0, 0.7);
-    border-radius: 31px;
-    justify-content: center;
-    margin: 10px 30vh;
-    margin-top: 25rem;
-    padding: 8px;
-    display: flex;
-    gap: 10px;
-  }
-
-  @media (max-width: 980px) {
-    .q-answer {
-      top: 0;
-    }
-  }
-
-  input[type="radio"] {
-    display: none;
-  }
-
-  label {
-    display: block;
-    cursor: pointer;
-    font-weight: 500;
-    margin-bottom: 10px;
-  }
-
-  label span{
-    text-transform: uppercase;
-    display: inline-flex;
-    align-items: center;
-    padding: 10px 20px 10px 10px;
-    border-radius: 31px;
-    color: #fff;
-    transition: 0.25s;
-  }
-
-
-  label span:hover,
-  input[type="radio"]:checked + span{
-    background: rgba(0, 0, 0, 0.8);
-    color: #fff;
-  }
-
-  label span:before {
-    visibility: hidden;
-  }
-
-  input[type="radio"]:checked + span:before {
-    box-shadow: inset 0 0 0 10px #00005c;
-  }
-
-  .send {
-    .btn{
-        background: rgba(0, 0, 0, 0.8);
-        padding: 10px 20px;
-        top: 1rem;
-        left: 50%;
-        font-size: 20px;
-        font-family: 'Montserrat';
-        text-decoration: none;
-        cursor: pointer;
-        border-radius: 10%;
-        transition: 0.8s;
-        position: relative;
-        z-index: 1;
-        overflow: hidden;
-        color: #fff;
-      }
-      .btn__link:hover{
-        background: #00afea;
-        color: rgba(0, 0, 0, 0.8);
-      }
-  }
-}
 /* Estilos generales */
 .show {
   display: flex;
@@ -294,29 +131,9 @@ export default {
   background: #74f339;
 }
 
-
-
-
-/* Responsive Styles */
-
-@media (max-width: 980px) {
-  .container {
-    margin-top: 5rem;
-    margin-left: 1rem;
-  }
-  
-
-  .send {
-    .btn {
-      .btn__link{
-        font-size: 10px;
-      }
-    }
-  }
-
-  label span{
-    font-size: 10px;
-  }
+.advanceProduct {
+  width: 100%;
+  height: 100%;
 }
 
 </style>
